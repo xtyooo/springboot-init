8e41dd9201d4555a98c585a21fa1d9468ce6d7c0
xtyooo <1286214601@qq.com>
2024-10-01 16:42:52 +0800

    初始模板

---
+++ springboot-init-master/src/main/java/com/xty/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.xty.springbootinit.mapper;
+
+import com.xty.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+*/
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
